2e533e3d1d057ce58b8f09c9750a5d590210211e
Aman Sumesh <amansumesh2005@gmail.com>
2026-03-02 17:34:34 +0530

    Add App and AppTest files

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,5 +1,37 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class AppTest {
-    public static void main(String[] args) {
-        System.out.println("Test");
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        App app = new App();
+        assertEquals(2, app.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        App app = new App();
+        assertEquals(15, app.multiply(5, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        App app = new App();
+        assertEquals(2, app.divide(6, 3));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        App app = new App();
+        app.divide(5, 0);
     }
 }
